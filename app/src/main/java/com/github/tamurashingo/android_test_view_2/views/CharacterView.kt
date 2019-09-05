965d1c3108d6b298d7c12512f68d4cf3adf1a3c2
tamura shingo <tamura.shingo@gmail.com>
2019-09-06 00:19:14 +0900

    dp2pxを追加

--- app/src/main/java/com/github/tamurashingo/android_test_view_2/views/CharacterView.kt
+++ app/src/main/java/com/github/tamurashingo/android_test_view_2/views/CharacterView.kt
@@ -24,8 +24,13 @@ class CharacterView : View {
     constructor(context: Context, attrs: AttributeSet?): super(context, attrs)
 
     override fun onDraw(canvas: Canvas?) {
-        canvas?.drawBitmap(bitmap, 0f, 0f, paint)
-        canvas?.drawBitmap(bitmap, 32f, 0f, paint)
+        canvas?.drawBitmap(bitmap, convertDp2Px(0f), convertDp2Px(0f), paint)
+        canvas?.drawBitmap(bitmap, convertDp2Px(32f), convertDp2Px(0f), paint)
+    }
+
+
+    fun convertDp2Px(dp: Float): Float {
+        return dp * context.resources.displayMetrics.density
     }
 
 }
